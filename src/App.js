42860907d951fcbfbc9dfa67f777d86665ce0a64
emcrusta <emcrusta@gmail.com>
2021-03-24 14:27:49 -0600

    add categories and dropdown option

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from './FlashcardList';
 import './app.css';
 
 function App() {
 	const [flashcards, setFlashcards] = useState([]);
+	const [categories, setCategories] = useState([]);
+	const categoryEl = useRef();
 
 	useEffect(() => {
-		fetch('./flashcards.json')
+		fetch('./categories.json')
+			.then(res => res.json())
+			.then(categories => setCategories(categories));
+	}, []);
+	// loads up our letter cards initially
+	useEffect(() => {
+		fetch('./letter-cards.json')
 			.then(res => res.json())
 			.then(flashcards => setFlashcards(flashcards));
 	}, []);
+
+	function handleSelect(event) {
+		event.preventDefault();
+		fetch(categoryEl.current.value)
+			.then(res => res.json())
+			.then(flashcards => setFlashcards(flashcards));
+	}
+
 	return (
 		<div>
+			<form className="input-form" action="">
+				<div className="form-group">
+					<label htmlFor="category">Select a Category</label>
+					<select
+						id="category"
+						ref={categoryEl}
+						onClick={handleSelect}
+					>
+						{categories.map(category => {
+							return (
+								<option value={category.file} key={category.id}>
+									{category.name}
+								</option>
+							);
+						})}
+					</select>
+				</div>
+			</form>
 			<FlashcardList flashcards={flashcards} />
 		</div>
 	);
